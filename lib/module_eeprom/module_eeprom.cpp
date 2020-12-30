5878ce7bdd00355cc2a8843e72f178b9d80c0190
hongthiet1994 <hongthiet1994@gmail.com>
2020-12-30 21:48:52 +0700

    fix bug eeprom empty for the first time

--- lib/module_eeprom/module_eeprom.cpp
+++ lib/module_eeprom/module_eeprom.cpp
@@ -37,4 +37,52 @@ void read_data_alarm()
             ui32_counter_addr++;            
         }        
     }
+    for ( i = 0; i < NUMBER_OF_ALARM; i++)
+    {
+        Serial.print("TIMER ");
+        Serial.println(i);
+        Serial.print("Hour : ");
+        Serial.println(data_alarm[i].data.hour);              
+        Serial.print("Minute : ");
+        Serial.println(data_alarm[i].data.minute);
+        Serial.print("Repeat : ");
+        Serial.println(data_alarm[i].data.repeat);
+        Serial.print("State : ");
+        Serial.println(data_alarm[i].data.state);
+        Serial.print("Wday_repeat : ");
+        Serial.println(data_alarm[i].data.wday_repeat);
+        if ((data_alarm[i].data.hour<0)||(data_alarm[i].data.hour>60))
+        {
+            data_alarm[i].data.hour = 0;
+        }   
+        if ((data_alarm[i].data.minute<0)||(data_alarm[i].data.minute>60))
+        {
+            data_alarm[i].data.minute = 0;
+        }
+        if ((data_alarm[i].data.repeat<0)||(data_alarm[i].data.repeat>1))
+        {
+            data_alarm[i].data.repeat = 0;
+        } 
+        if ((data_alarm[i].data.state<0)||(data_alarm[i].data.state>1))
+        {
+            data_alarm[i].data.state = 0;
+        }  
+        if ((data_alarm[i].data.wday_repeat<0)||(data_alarm[i].data.wday_repeat>7))
+        {
+            data_alarm[i].data.wday_repeat = 0;
+        } 
+        write_data_alarm();  
+    }
+    
+    // if (data_alarm[0].data.hour>60)
+    // {
+    //     data_alarm[0].data.hour = 01;
+    //     data_alarm[0].data.minute = 01;   
+    //     data_alarm[0].data.repeat = 0;
+    //     data_alarm[0].data.state = 0; 
+    //     data_alarm[0].data.wday_repeat = 0;  
+    //     write_data_alarm();   
+    // }
+   
+
 }
